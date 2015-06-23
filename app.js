a985eabe9cf4c17ef44745bf6507615525539bf9
Arman Jon Villalobos <me@armanjon.com>
2015-06-23 17:32:08 +0800

    added body-parser and fix api call bugs

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 var express = require('express');
 var request = require('request');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 
 // Declare server object
 var app = express();
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
 
 var TOKEN = process.env.TOKEN;
 var DOMAIN = process.env.DOMAIN;
@@ -16,6 +18,19 @@ app.get('/api/v1/users/', function (req, res) {
   var users = request.get(url, function (error, response, body) {
     var users_collection = JSON.parse(body);
       res.json(users_collection.members.length);
+  });
+});
+
+app.post('/api/v1/invite/', function (req, res) {
+  if (!req.body.hasOwnProperty('email') || (!req.body.hasOwnProperty('time'))) {
+    res.statusCode = 400;
+    return res.send('Error 400: Parameters incorrect!');
+  }
+
+  var url = "https://" + process.env.DOMAIN + ".slack.com/api/users.admin.invite?t=" + req.body.time + "&email=" + req.body.email + "&token=" + process.env.TOKEN + "&set_active=true";
+
+  var result = request.post(url, function (error, response, body) {
+    res.json(JSON.parse(body));
   });
 });
 
