5f9a06b263650784c12005591a9b9479c5842f48
Arman Jon Villalobos <me@armanjon.com>
2015-06-25 17:15:12 +0800

    change post request to be under express router

--- app.js
+++ app.js
@@ -13,26 +13,51 @@ var TOKEN = process.env.TOKEN;
 var DOMAIN = process.env.DOMAIN;
 
 // Declare routes
-app.get('/api/v1/users/', function (req, res) {
-  var url = "https://"+ DOMAIN +".slack.com/api/users.list?token=" + TOKEN;
-  var users = request.get(url, function (error, response, body) {
-    var users_collection = JSON.parse(body);
-      res.json(users_collection.members.length);
-  });
-});
 
-app.post('/api/v1/invite/', function (req, res) {
-  if (!req.body.hasOwnProperty('email') || (!req.body.hasOwnProperty('time'))) {
-    res.statusCode = 400;
-    return res.send('Error 400: Parameters incorrect!');
-  }
+var apiRouter = express.Router();
 
-  var url = "https://" + process.env.DOMAIN + ".slack.com/api/users.admin.invite?t=" + req.body.time + "&email=" + req.body.email + "&token=" + process.env.TOKEN + "&set_active=true";
+apiRouter.route('/api/v1/')
+    .get('/users', function (req, res) {
+      var url = "https://" + DOMAIN + ".slack.com/api/users.list?token=" + TOKEN;
+      var users = request.get(url, function (error, response, body) {
+        var users_collection = JSON.parse(body);
+        res.json(users_collection.members.length);
+      });
+    })
 
-  var result = request.post(url, function (error, response, body) {
-    res.json(JSON.parse(body));
-  });
-});
+    .post('/invite', function (req, res) {
+      if (!req.body.hasOwnProperty('email') || (!req.body.hasOwnProperty('time'))) {
+        res.statusCode = 400;
+        return res.send('Error 400: Parameters incorrect!');
+      }
+
+      var url = "https://" + process.env.DOMAIN + ".slack.com/api/users.admin.invite?t=" + req.body.time + "&email=" +
+              req.body.email + "&token" + process.env.TOKEN + "&set_active=true";
+      var result = request.post(url, function (error, response, body) {
+        res.json(JSON.parse(body));
+      });
+    });
+
+//app.get('/api/v1/users/', function (req, res) {
+//  var url = "https://"+ DOMAIN +".slack.com/api/users.list?token=" + TOKEN;
+//  var users = request.get(url, function (error, response, body) {
+//    var users_collection = JSON.parse(body);
+//      res.json(users_collection.members.length);
+//  });
+//});
+
+//app.post('/api/v1/invite/', function (req, res) {
+//  if (!req.body.hasOwnProperty('email') || (!req.body.hasOwnProperty('time'))) {
+//    res.statusCode = 400;
+//    return res.send('Error 400: Parameters incorrect!');
+//  }
+//
+//  var url = "https://" + process.env.DOMAIN + ".slack.com/api/users.admin.invite?t=" + req.body.time + "&email=" + req.body.email + "&token=" + process.env.TOKEN + "&set_active=true";
+//
+//  var result = request.post(url, function (error, response, body) {
+//    res.json(JSON.parse(body));
+//  });
+//});
 
 // Spawn server instance
 app.listen(process.env.PORT || 8888);
