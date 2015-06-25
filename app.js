d6e12337e7c7c94e89f57e8ce5e99e7b1143798f
Arman Jon Villalobos <me@armanjon.com>
2015-06-25 17:25:42 +0800

    deleted express router code

--- app.js
+++ app.js
@@ -14,24 +14,14 @@ var DOMAIN = process.env.DOMAIN;
 
 // Declare routes
 
-var apiRouter = express.Router();
 
-apiRouter.route('/api/v1/')
-    .get('/users', function (req, res) {
-      var url = "https://" + DOMAIN + ".slack.com/api/users.list?token=" + TOKEN;
-      var users = request.get(url, function (error, response, body) {
-        var users_collection = JSON.parse(body);
-        res.json(users_collection.members.length);
-      });
-    });
-
-//app.get('/api/v1/users/', function (req, res) {
-//  var url = "https://"+ DOMAIN +".slack.com/api/users.list?token=" + TOKEN;
-//  var users = request.get(url, function (error, response, body) {
-//    var users_collection = JSON.parse(body);
-//      res.json(users_collection.members.length);
-//  });
-//});
+app.get('/api/v1/users/', function (req, res) {
+  var url = "https://"+ DOMAIN +".slack.com/api/users.list?token=" + TOKEN;
+  var users = request.get(url, function (error, response, body) {
+    var users_collection = JSON.parse(body);
+      res.json(users_collection.members.length);
+  });
+});
 
 app.post('/api/v1/invite/', function (req, res) {
   if (!req.body.hasOwnProperty('email') || (!req.body.hasOwnProperty('time'))) {
