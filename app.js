0c519c0b8e0c168d989ccd6d1005c2a7ba1a98eb
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-06-25 17:31:25 +0800

    Pass POST parameters via form data

--- app.js
+++ app.js
@@ -29,9 +29,15 @@ app.post('/api/v1/invite/', function (req, res) {
     return res.send('Error 400: Parameters incorrect!');
   }
 
-  var url = "https://" + process.env.DOMAIN + ".slack.com/api/users.admin.invite?t=" + req.body.time + "&email=" + req.body.email + "&token=" + process.env.TOKEN + "&set_active=true";
+  var url = "https://" + process.env.DOMAIN + ".slack.com/api/users.admin.invite";
+  var options = {
+    t: req.body.time,
+    email: req.body.email,
+    token: process.env.TOKEN,
+    set_active: true
+  };
 
-  var result = request.post(url, function (error, response, body) {
+  var result = request.post({ url: url, form: options }, function (error, response, body) {
     res.json(JSON.parse(body));
   });
 });
